a9554a18a18af9d5bb7cbee1323ad1755332c289
Amir Moemen <amir.moemen@gmail.com>
2020-12-07 22:27:43 +0100

    Day 7

---
+++ 2020/Solutions/Year2020/Day07/Bag.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Solutions.Year2020.Day07
+{
+    public class Bag
+    {
+        public int Count { get; set; }
+        public string Color { get; set; }
+    }
+}
