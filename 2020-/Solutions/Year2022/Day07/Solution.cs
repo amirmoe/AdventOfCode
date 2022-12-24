e3c1d9178a62e33d0b06c7101b25c1682286205a
Amir Moemen <amir.moemen@gmail.com>
2022-12-24 14:18:52 +0100

    Byter dator

---
+++ 2020-/Solutions/Year2022/Day07/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode.Solutions.Year2022.Day07;
+
+internal class Day07 : ASolution
+{
+    public Day07() : base(07, 2022, "No Space Left On Device",  true)
+    {
+    }
+
+    protected override string SolvePartOne()
+    {
+
+        var rows = new Queue<string>(Input.SplitByNewline().Skip(1));
+
+        while (rows.Any())
+        {
+            var row = rows.Dequeue();
+            
+            if (row.Contains("$ ls"))
+            {
+                var filesInFolder = new List<string>();
+                while (rows.Any() || !rows.Peek().Contains('$'))
+                {
+                    filesInFolder.Add(rows.Dequeue());
+                }
+            }
+
+
+
+        }
+        
+        
+        foreach (var row in rows)
+        {
+            if (row.Contains("$ ls"))
+            {
+                
+            }
+            
+            
+        }
+        
+        
+        return "";
+    }
+
+    protected override string SolvePartTwo()
+    {
+
+        return "";
+    }
+}
